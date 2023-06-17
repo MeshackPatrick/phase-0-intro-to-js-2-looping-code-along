252e3165576821f3590e3a9f742e21d4203295e1
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-06-17 09:37:52 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+const result = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(result);
+
+
+
+
+function countDown(integers){
+  while (integers >=0){
+    console.log (integers);
+    integers--;
+  }
+}countDown(10);
